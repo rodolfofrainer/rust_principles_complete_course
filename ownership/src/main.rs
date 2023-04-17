3975d0e28572fc3a91ce300746f1f8324f0bfae6
Rodolfo Frainer <91473305+rodolfofrainer@users.noreply.github.com>
2023-04-17 13:35:07 +0100

    RUST PRINCIPLES - CLONE

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -10,12 +10,33 @@
 // var is dropped, heap is dropped
 
 // RUST PRINCIPLES - MOVE
+// fn main(){
+//     let x = vec!["Tyler".to_string()];
+//     let y = x;
+//     // println!("{:?}", x);
+//     //error[E0382]: borrow of moved value: `x`
+//     let z = y;
+//     println!("{:?}", z);
+
+// }
+
+// RUST PRINCIPLES - CLONE
+// fn main(){
+//     let x = vec!["Tyler".to_string()];
+//     let y = x.clone();
+//     let z = y.clone();
+//     println!("{:?}", x);
+//     println!("{:?}", y);
+//     println!("{:?}", z);
+// }
+
+// RUST PRINCIPLES - COPY
+
 fn main(){
     let x = vec!["Tyler".to_string()];
-    let y = x;
-    // println!("{:?}", x);
-    //error[E0382]: borrow of moved value: `x`
-    let z = y;
+    let y = x.clone();
+    let z = y.clone();
+    println!("{:?}", x);
+    println!("{:?}", y);
     println!("{:?}", z);
-
 }
